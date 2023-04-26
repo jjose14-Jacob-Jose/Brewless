373a0fa5c555ec2b0453ff63285c527fb9140f6e
MJKangas <97559538+MJKangas@users.noreply.github.com>
2023-04-25 20:40:10 -0700

    Update brewlessParser.pl
    Added Additional check for boolean values and setup for checking expressions.

--- src/compiler/brewlessParser.pl
+++ src/compiler/brewlessParser.pl
@@ -27,3 +27,57 @@ expression_eval(B, Env, OutEnv) :- boolean_eval(B, Env, OutEnv).
 expression_eval(t_plus(A,B), Env, OutEnv) :- expression_eval(A, Env, Env1), term_eval(B, Env1, OutEnv).
 expression_eval(t_minus(A,B), Env, OutEnv) :- expression_eval(A, Env, Env1), term_eval(B, Env1, OutEnv).
 expression_eval(A, Env, OutEnv) :- expression_eval(A, Env, OutEnv).
+
+term_eval(t_times(A, B), Env, OutEnv) :- term_eval(A, Env, Env1), paren_eval(B, Env1, OutEnv).
+term_eval(t_divide(A, B), Env, OutEnv) :- term_eval(A, Env, Env1), paren_eval(B, Env1, OutEnv).
+term_eval(A, Env, OutEnv) :- paren_eval(A, Env, OutEnv).
+
+paren_eval(t_paren(A), Env, OutEnv) :- expression_eval(A, Env, OutEnv).
+paren_eval(A, Env, OutEnv) :- value_eval(A, Env, OutEnv).
+
+value_eval(A, Env, OutEnv) :- identifier_eval(A, Env, OutEnv).
+value_eval(A, Env, OutEnv) :- integer_eval(A, Env, OutEnv).
+
+integer_eval(I, Env, OutEnv) :- [I], {is_of_type(integer, I)}.
+
+identifier_eval(L, Env, OutEnv) :- letter_eval(L, Env, OutEnv).
+%Might want to change how we do our identifiers.
+%letter_eval(A, Env, OutEnv) :- 
+
+bool_eval(t_bool(true), _, _, true). 
+bool_eval(t_bool(false), _, _, false).
+bool_eval(t_bool(E1, '==', E2), Env, OutEnv, true) :- E1 =:= E2.
+bool_eval(t_bool(E1, '==', E2), Env, OutEnv, false) :- E1 =/= E2.
+bool_eval(t_bool(not, B), Env, OutEnv, Result):- bool_eval(B, Env, OutEnv, Temp), bool_not(Temp, Result).
+bool_eval(t_bool(B1, and, B2), Env, OutEnv, Result) :- bool_eval(B1, Env, OutEnv, Temp1), bool_eval(B2, Env, OutEnv, Temp2), bool_check(Temp1, and, Temp2, Result). 
+bool_eval(t_bool(B1, or, B2), Env, OutEnv, Result) :- bool_eval(B1, Env, OutEnv, Temp1), bool_eval(B2, Env, OutEnv, Temp2), bool_check(Temp1, or, Temp2, Result). 
+bool_eval(t_ident(I1, '<', I2), Env, OutEnv, true) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 < Temp2.
+bool_eval(t_ident(I1, '<', I2), Env, OutEnv, false) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 >= Temp2.
+bool_eval(t_ident(I1, '<=', I2), Env, OutEnv, true) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 <= Temp2.
+bool_eval(t_ident(I1, '<=', I2), Env, OutEnv, false) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 > Temp2.
+bool_eval(t_ident(I1, '>', I2), Env, OutEnv, true) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 > Temp2.
+bool_eval(t_ident(I1, '>', I2), Env, OutEnv, false) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 <= Temp2.
+bool_eval(t_ident(I1, '>=', I2), Env, OutEnv, true) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 >= Temp2.
+bool_eval(t_ident(I1, '>=', I2), Env, OutEnv, false) :- identifier_eval(I1, Env, OutEnv, Temp1), identifier_eval(I2, Env, OutEnv, Temp2), Temp1 < Temp2.
+
+bool_not(true, false).
+bool_not(false, true).
+
+bool_check(true , and, true  , true).
+bool_check(true , and, false , false).
+bool_check(false , and, true  , false).
+bool_check(false , and, false , false).
+bool_check(true , or , true  , true).
+bool_check(true , or , false , true).
+bool_check(false , or , true  , true).
+bool_check(false , or , false , false).
+
+
+%Taken From Assign 3 Semantics, needs editing
+lookup(Identifier, [(Identifier, Number) | _], Number).
+lookup(Identifier, [_ | T], Number):- lookup(Identifier, T, Number).
+
+update(Identifier, [(Identifier, _) | T], Number, [(Identifier, Number) | T]).
+update(Identifier, [H | T], Number, [H | Env]):- update(Identifier, T, Number, Env).
+
+setEnv((Identifier, Number), Env, Result):- EnvTemp = [(Identifier, Number)], append(Env, EnvTemp, Result).
