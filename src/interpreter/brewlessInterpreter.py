db1bbeecc06890f7bbfa1321a4c8a34f8c091d67
Jacob Jose <jjose14@asu.edu>
2023-04-27 02:41:43 -0700

    Interpeter query results prototype working.

--- src/interpreter/brewlessInterpreter.py
+++ src/interpreter/brewlessInterpreter.py
@@ -16,7 +16,6 @@ from pyswip import Prolog
 # program(P, [begin, var, z, ; , var, x, ;, z, :=, x, end, .], []), program_eval(P, 2, 3, Z).
 
 
-
 # prolog = Prolog()
 # prolog.consult("assignSemantics.pl")
 # input_Program= "[begin, var, z, ; , var, x, ;, z, :=, x, end, .]"
@@ -24,7 +23,7 @@ from pyswip import Prolog
 # print("queryString: ", queryString)
 # for result in prolog.query(queryString):
 #     print(result["P"])
-    
+
 #     parseTree = result["P"]
 #     print("parsetree: " + parseTree)
 #     x = "2"
@@ -36,12 +35,73 @@ from pyswip import Prolog
 #             print(result["Z"])
 
 
-prolog = Prolog()
-prolog.consult("assignSemantics.pl")
-input_Program= "[begin, var, z, ; , var, x, ;, z, :=, x, end, .]"
-queryString = "execute_program( " + input_Program  + ", Z)."
-print("queryString: ", queryString)
-resultOne = prolog.query(queryString)
-for result in resultOne:
-    print(result["Z"])
+# prolog = Prolog()
+# prolog.consult("assignSemantics.pl")
+# input_Program= "[begin, var, z, ; , var, x, ;, z, :=, x, end, .]"
+# queryString = "execute_program( " + input_Program  + ", Z)."
+# print("queryString: ", queryString)
+# resultOne = prolog.query(queryString)
+# for result in resultOne:
+#     print(result["Z"])
 
+import sys
+
+MSG_MISSING_COMMAND_LINE_ARGUMENTS = "Please enter 3 commandline arguments in the format <prolog_file_name> <prolog_query> <output_variable>."
+MSG_ERROR_WHILE_RUNNING_PROLOG = "There was an error while executing the program. Please check the program's syntax and calling parameters. "
+
+
+def getCommandLineArgumentsAndCompileTokens():
+    # Get the command line arguments
+    listCommandLineArguments = sys.argv
+
+    try:
+        # Prolog 'filepath', 'predicate name' and 'ouput variable name' must be mentioned in this sequence. 
+        prologFileName = listCommandLineArguments[1]
+        prologCommandOrQuery = listCommandLineArguments[2]
+        prologOutputVariable = listCommandLineArguments[3]
+
+        return compileTokens(prologFileName, prologCommandOrQuery, prologOutputVariable)
+    except:
+        print(MSG_MISSING_COMMAND_LINE_ARGUMENTS)
+
+
+def compileTokens(prologFilePath, prologCommandOrQuery, prologOutputVariable):
+    try:
+        # Create a Prolog object.
+        prolog = Prolog()
+
+        # Consult the Prolog file.
+        prolog.consult(prologFilePath)
+
+        # Call the Prolog predicate.
+        listPrologResults = prolog.query(prologCommandOrQuery)
+
+        # Return Prolog query result.
+        # Prolog query result is list had multiple instances of same value. So only first value chose.
+        return getFirstResult(listPrologResults, prologOutputVariable)
+        # return getAllResult(listPrologResults, prologOutputVariable)
+
+    except:
+        print(MSG_ERROR_WHILE_RUNNING_PROLOG)
+
+
+# This method only returns first result of Prolog query.
+def getFirstResult(listPrologResults, prologOutputVariable):
+    if len(listPrologResults) > 0:
+        return listPrologResults[0][prologOutputVariable]
+
+
+# This method only returns all result of Prolog query.    
+def getAllResult(listPrologResults, prologOutputVariable):
+    listResults = []
+    for prologResult in listPrologResults:
+        listResults.append(prologResult[prologOutputVariable])
+    return listResults
+
+
+def main():
+    print(getCommandLineArgumentsAndCompileTokens())
+
+
+if __name__ == "__main__":
+    main()
