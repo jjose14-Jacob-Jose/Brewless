1870d9de5e6c3bfca7a56d043bf616dfd73a80aa
Jacob Jose <jjose14@asu.edu>
2023-04-27 03:11:27 -0700

    Removed old files. Updated Readme.md.

--- src/interpreter/sample.pl
+++
@@ -1,4 +0,0 @@
-father(alice, bob).
-father(alice, charlie).
-father(alice, dennis).
-father(elizabeth, frank).
--- src/interpreter_java/src/main/java/interpreter/sample.pl
+++
@@ -1,6 +0,0 @@
-:- dynamic father/2.
-
-father(alice, bob).
-father(alice, charlie).
-father(bob, dennis).
-father(bob, elizabeth).
