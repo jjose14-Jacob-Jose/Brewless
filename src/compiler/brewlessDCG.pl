99aab187c22b1afc0e5d156db21bdececbd025ee
Tahir Pervez <tpervez@asu.edu>
2023-04-27 16:12:07 -0700

    added test loader, trying to get conditionals right

--- src/compiler/brewlessDCG.pl
+++ src/compiler/brewlessDCG.pl
@@ -1,11 +1,27 @@
 :- use_rendering(svgtree).
 :- table expr/3, term/3, bool/3.
 
+test(1,['{',int,"a",;,'}']).
+test(2,['{',int,"a",;,int,b,=,3,;,'}']).
+test(3,['{',int,"a",;,"a",=,3,;,'}']).
+test(4,['{',int, "a",;,if, '(',true,')', '{',"a", =, 5,;,'}',;,'}']).
+test(5,['{',int, "a",;,if, '(',false,')', '{',"a", =, 5,;,'}',else,'{',"a",=,3,'}',;,'}']).
+test(6,['{',for,'(',int,i,=,0,;,i,<,10,;,i,++,')','{',print,'(',i,')',;,'}',;,'}']).
+test(7,['{',for,'(',int,i,=,10,;,i,>,0,;,i,--,')','{',int,"a",=,i,;,'}',;,'}']).
+test(8,['{',for,'(',int,i,=,0,;,i,<,10,;,i,=,i,+,2,')','{',int,"a",=,i,;,'}',;,'}']).
+test(9,['{',for, '(',i, in, 'Range','(',3,',',7,')',')', '{',int, "a", =, i,;,'}',;,'}']).
+test(10,['{',int, "a", =, 3,;,while,'(',"a", '<', 5,')', '{',"a", =, "a", +, 1,;,'}',;,'}']).
+test(11,['{',int, "a", =, 3,;,do, '{',"a", =, "a", +, 1,;,'}', while, '(', "a", '>=', 5, ')', ;,'}']).
+test(12,['{',int, "a", =, 3,;,int, b, =, 2,;,"a", ==, b, ?, "a", =, "a", +, b, :, "a", =, b,;,'}']).
+test(13,['{','String',"apple",=,'"',"apple",'"',;,'}']).
+test(14,['{',boolean,"a",=,true,or,false,;,'}']).
+test(15,['{',boolean,"a",=,true,and,false,;,'}']).
+
 program(t_prog(B)) --> ['{'], block(B), [;], ['}'].
 
 block(t_block(C,B)) --> cmd(C), [;], block(B).
 block(C) --> cmd(C).
-
+/*
 cmd(D) --> dec(D).
 cmd(A) --> assign(A).
 cmd(C) --> cond(C).
@@ -13,13 +29,15 @@ cmd(F) --> for(F).
 cmd(W) --> while(W).
 cmd(T) --> tern(T).
 cmd(P) --> print(P).
+*/
+cmd(X) --> dec(X) | assign(X) | cond(X) | for(X) | while(X) | tern(X) | print(X).
 
 dec(t_dec(T,I)) --> type(T), ident(I).
-dec(t_dec(T,A)) --> type(T), assign(A).
+dec(t_dec_ass(T,A)) --> type(T), assign(A).
 
-type(t_type(int)) --> [int].
-type(t_type(string)) --> ['String'].
-type(t_type(bool)) --> [bool].
+type(t_type_int(int)) --> [int].
+type(t_type_string(string)) --> ['String'].
+type(t_type_bool(bool)) --> [bool].
 
 assign(t_assign(I,E)) --> ident(I), [=], expr(E).
 
@@ -56,16 +74,16 @@ letter(A) -->
     ['w'], {A='w'} | ['W'], {A='W'} | ['x'], {A='x'} | ['X'], {A='X'} | 
     ['y'], {A='y'} | ['Y'], {A='Y'} | ['z'], {A='z'} | ['Z'], {A='Z'}.
 */
-bool(t_bool(true)) --> [true].
-bool(t_bool(false)) --> [false].
-bool(t_bool(E1,E2)) --> expr(E1), ['=='], expr(E2).
-bool(t_bool(B)) --> [not], bool(B).
-bool(t_bool(B1,B2)) --> bool(B1), [and], bool(B2).
-bool(t_bool(B1,B2)) --> bool(B1), [or], bool(B2).
-bool(t_bool(I1,I2)) --> ident(I1), [<], int(I2).
-bool(t_bool(I1,I2)) --> ident(I1), [<=], int(I2).
-bool(t_bool(I1,I2)) --> ident(I1), [>], int(I2).
-bool(t_bool(I1,I2)) --> ident(I1), [>=], int(I2).
+bool(t_bool_true(true)) --> [true].
+bool(t_bool_false(false)) --> [false].
+bool(t_bool_comp(E1,E2)) --> expr(E1), ['=='], expr(E2).
+bool(t_bool_not(B)) --> [not], bool(B).
+bool(t_bool_and(B1,B2)) --> bool(B1), [and], bool(B2).
+bool(t_bool_or(B1,B2)) --> bool(B1), [or], bool(B2).
+bool(t_bool_lt(I1,I2)) --> ident(I1), [<], int(I2).
+bool(t_bool_lteq(I1,I2)) --> ident(I1), [<=], int(I2).
+bool(t_bool_gt(I1,I2)) --> ident(I1), [>], int(I2).
+bool(t_bool_gteq(I1,I2)) --> ident(I1), [>=], int(I2).
 
 string(t_str(I1, I2)) --> ["string"], ident(I1), [=], ['"'], ident(I2), ['"'].
 
