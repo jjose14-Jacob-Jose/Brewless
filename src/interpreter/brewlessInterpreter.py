83dbf3a774e73778e9debc1e1a5cefb29b299bcd
Jacob Jose <jjose14@asu.edu>
2023-04-27 02:54:20 -0700

    Fixed Python -> Prolog program.

--- src/interpreter/brewlessInterpreter.py
+++ src/interpreter/brewlessInterpreter.py
@@ -1,49 +1,5 @@
 from pyswip import Prolog
 
-# prolog = Prolog()
-# prolog.consult("assignSemantics.pl")
-# P= "[begin, var, z, ; , var, x, ;, z, :=, x, end, .]"
-# for result in prolog.query("program_eval( [begin, var, z, ; , var, x, ;, z, :=, x, end, .], 2, 3, Z)"):
-#     print(result["P"], result["Z"])
-
-# prolog = Prolog()
-# prolog.consult("sample.pl")
-# name = "alice"
-# queryString = ("father(" + name + ", Z)")
-# for result in prolog.query(queryString):
-#     print(result["Z"])
-
-# program(P, [begin, var, z, ; , var, x, ;, z, :=, x, end, .], []), program_eval(P, 2, 3, Z).
-
-
-# prolog = Prolog()
-# prolog.consult("assignSemantics.pl")
-# input_Program= "[begin, var, z, ; , var, x, ;, z, :=, x, end, .]"
-# queryString = "program( P, "+ input_Program + ", [])."
-# print("queryString: ", queryString)
-# for result in prolog.query(queryString):
-#     print(result["P"])
-
-#     parseTree = result["P"]
-#     print("parsetree: " + parseTree)
-#     x = "2"
-#     y = "3"
-#     queryTwo = "program_eval("+parseTree+", " + x + ", " + y + ", Z)."
-#     print("queryTwo: ", queryTwo)
-
-#     for resultTwo in prolog.query(queryTwo):
-#             print(result["Z"])
-
-
-# prolog = Prolog()
-# prolog.consult("assignSemantics.pl")
-# input_Program= "[begin, var, z, ; , var, x, ;, z, :=, x, end, .]"
-# queryString = "execute_program( " + input_Program  + ", Z)."
-# print("queryString: ", queryString)
-# resultOne = prolog.query(queryString)
-# for result in resultOne:
-#     print(result["Z"])
-
 import sys
 
 MSG_MISSING_COMMAND_LINE_ARGUMENTS = "Please enter 3 commandline arguments in the format <prolog_file_name> <prolog_query> <output_variable>."
@@ -77,26 +33,24 @@ def compileTokens(prologFilePath, prologCommandOrQuery, prologOutputVariable):
         listPrologResults = prolog.query(prologCommandOrQuery)
 
         # Return Prolog query result.
-        # Prolog query result is list had multiple instances of same value. So only first value chose.
-        return getFirstResult(listPrologResults, prologOutputVariable)
+        # Prolog query result had multiple instances of same value. Set 'flagGetAllResults' as 'false' to get only first value.
+        flagGetAllResults = False
+        return getPrologQuery(listPrologResults, prologOutputVariable, flagGetAllResults)
         # return getAllResult(listPrologResults, prologOutputVariable)
 
     except:
         print(MSG_ERROR_WHILE_RUNNING_PROLOG)
 
-
-# This method only returns first result of Prolog query.
-def getFirstResult(listPrologResults, prologOutputVariable):
-    if len(listPrologResults) > 0:
-        return listPrologResults[0][prologOutputVariable]
-
-
-# This method only returns all result of Prolog query.    
-def getAllResult(listPrologResults, prologOutputVariable):
-    listResults = []
+# This method only returns results of Prolog query.
+def getPrologQuery(listPrologResults, prologOutputVariable, flagGetAllResults):
+    list_results = []
     for prologResult in listPrologResults:
-        listResults.append(prologResult[prologOutputVariable])
-    return listResults
+        list_results.append(prologResult[prologOutputVariable])
+
+        # Return only first result if 'flagGetAllResults' is false.
+        if not flagGetAllResults:
+            break
+    return list_results
 
 
 def main():
@@ -105,3 +59,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# To run this class from Ubuntu Terminal:
+#     python3 brewlessInterpreter.py assignSemantics.pl "execute_program([begin, var, z, ; , var, x, ;, z, :=, x, end, .], Z)." "Z"
