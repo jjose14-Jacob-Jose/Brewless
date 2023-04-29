c9ecbb58a066647512ee8eec5c420b56d151aa71
Tahir Pervez <tpervez@asu.edu>
2023-04-28 17:54:15 -0700

    reset to previous branch, while works, tern does not

--- src/compiler/brewlessDCG.pl
+++ src/compiler/brewlessDCG.pl
@@ -1,50 +1,6 @@
 :- use_rendering(svgtree).
 :- table expr/3, term/3, bool/3.
 
-test(cmd,['{',int,"a",;,'}']).
-test(cmdlst,['{',int,"a",;,int,"b",=,3,;,'}']).
-test(assign,['{',int,"a",;,"a",=,3,;,'}']).
-test(if,['{',
-            int, "a",;,
-            if, '(',true,')', '{',"a", =, 5,;,'}',;,
-          '}']).
-test(if_else,['{',
-                 int, "a",;,
-                 if, '(',false,')', '{',"a", =, 5,;,'}',
-                 else,'{',"a",=,3,;,'}',;,'}']).
-test(for_inc,['{', 
-                 for, '(', int, "i", =, 0,;, "i", <, 10,;, "i", ++, ')', 
-                 '{', print, '(', "i", ')', ;, '}', ;, 
-             '}']).
-test(for_dec,['{', 
-                 for, '(', int, "i", =, 0,;, "i", <, 10,;, "i", --, ')', 
-                 '{', print, '(', "i", ')', ;, '}', ;, 
-             '}']).
-test(for_assign,['{', 
-                  for, '(', int, "i", =, 10,;, "i", >, 0,;, "i", =, "i",-,2, ')', 
-                  '{', print, '(', "i", ')', ;, '}', ;,
-                '}']).
-test(for_rng,['{',
-                 int, "i", =, 3,;,
-                 for, '(', "i", in, 'Range', '(', 2, ',', 7, ')', ')',
-                 '{', print, '(', "i", ')', ;, '}', ;,
-               '}']).
-test(while,['{',
-                int, "start", =, 1,;,
-                int, "end", =, 100,;,
-                while, '(', "start", <=, "end", ')', '{',
-                    print, '(', "start", ')',;,
-                    "start", =, "start", *, 2,;,
-                '}',;,
-            '}']).
-/*test(do_while,['{',
-                  do, '{',
-                      print, '(', '"',",bigger,",'"', ')',;,
-                  '}', while, '(', 3, >, 5, ')',;,
-              '}']).
-*/
-
-
 program(t_prog(B)) --> ['{'], block(B), ['}'].
 
 block(t_block(C,B)) --> cmd(C), [;], block(B).
@@ -68,10 +24,6 @@ type(t_type_bool(bool)) --> [bool].
 
 assign(t_assign(I,E)) --> ident(I), [=], expr(E).
 
-expr(I) --> ident(I).
-expr(I) --> int(I).
-expr(S) --> string(S).
-expr(B) --> bool(B).
 expr(t_plus(A, B)) --> expr(A), [+], term(B).
 expr(t_minus(A, B)) --> expr(A), [-], term(B).
 expr(A) --> term(A).
@@ -80,37 +32,20 @@ term(t_divide(A, B)) --> term(A), [/], paren(B).
 term(A) --> paren(A).
 paren(t_paren(A)) --> ['('], expr(A), [')'].
 paren(A) --> value(A).
-value(A) --> ident(A) | int(A).
+value(A) --> ident(A) | int(A) | string(A) | bool(A).
 
 int(I) --> [I], {is_of_type(integer, I)}.
 
 ident(I) --> [I], {is_of_type(string, I)}.
-/*
-letter(A) -->
-    ['a'], {A='a'} | ['A'], {A='A'} | ['b'], {A='b'} | ['B'], {A='B'} |
-    ['c'], {A='c'} | ['C'], {A='C'} | ['d'], {A='d'} | ['D'], {A='D'} | 
-    ['e'], {A='e'} | ['E'], {A='E'} | ['f'], {A='f'} | ['F'], {A='F'} | 
-    ['g'], {A='g'} | ['G'], {A='G'} | ['h'], {A='h'} | ['H'], {A='H'} |
-    ['i'], {A='i'} | ['I'], {A='I'} | ['j'], {A='j'} | ['J'], {A='J'} | 
-    ['k'], {A='k'} | ['K'], {A='K'} | ['l'], {A='l'} | ['L'], {A='L'} | 
-    ['m'], {A='m'} | ['M'], {A='M'} | ['n'], {A='n'} | ['N'], {A='N'} |
-    ['o'], {A='o'} | ['O'], {A='O'} | ['p'], {A='p'} | ['P'], {A='P'} |
-    ['q'], {A='q'} | ['Q'], {A='Q'} | ['r'], {A='r'} | ['R'], {A='R'} |
-    ['s'], {A='s'} | ['S'], {A='S'} | ['t'], {A='t'} | ['T'], {A='T'} |
-    ['u'], {A='u'} | ['U'], {A='U'} | ['v'], {A='v'} | ['V'], {A='V'} |
-    ['w'], {A='w'} | ['W'], {A='W'} | ['x'], {A='x'} | ['X'], {A='X'} | 
-    ['y'], {A='y'} | ['Y'], {A='Y'} | ['z'], {A='z'} | ['Z'], {A='Z'}.
-*/
+
 bool(t_bool_true(true)) --> [true].
 bool(t_bool_false(false)) --> [false].
 bool(t_bool_comp(E1,E2)) --> expr(E1), ['=='], expr(E2).
 bool(t_bool_not(B)) --> [not], bool(B).
 bool(t_bool_and(B1,B2)) --> bool(B1), [and], bool(B2).
 bool(t_bool_or(B1,B2)) --> bool(B1), [or], bool(B2).
-bool(t_bool_lt(I1,I2)) --> ident(I1), [<], int(I2).
-bool(t_bool_lteq(I1,I2)) --> ident(I1), [<=], int(I2).
-bool(t_bool_gt(I1,I2)) --> ident(I1), [>], int(I2).
-bool(t_bool_gteq(I1,I2)) --> ident(I1), [>=], int(I2).
+bool(t_bool_lt(I1,I2)) --> expr(I1), [<], expr(I2).
+bool(t_bool_gt(I1,I2)) --> expr(I1), [>], expr(I2).
 
 string(t_str(I1, I2)) --> ["string"], ident(I1), [=], ['"'], ident(I2), ['"'].
 
